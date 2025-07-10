8ddb7f1c46338b4f210801d404afc269857aa307
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com>
2025-07-10 14:04:11 +0500

    commit

--- project/src/index.js
+++ project/src/index.js
@@ -1,30 +1,11 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles.css";
 
-import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
-
-const data = [
-  { name: "Mon", focus: 3 },
-  { name: "Tue", focus: 5 },
-  { name: "Wed", focus: 2 },
-  { name: "Thu", focus: 4 },
-  { name: "Fri", focus: 6 },
-  { name: "Sat", focus: 1 },
-  { name: "Sun", focus: 3 }
-];
-
-const FocusChart = () => {
-  return (
-    <div style={{ width: "100%", height: 300 }}>
-      <ResponsiveContainer>
-        <BarChart data={data}>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Bar dataKey="focus" fill="#8884d8" />
-        </BarChart>
-      </ResponsiveContainer>
-    </div>
-  );
-};
-
-export default FocusChart;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
